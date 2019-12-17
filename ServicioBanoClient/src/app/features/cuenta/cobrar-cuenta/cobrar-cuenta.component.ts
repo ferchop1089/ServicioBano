623d2300bc504aa79aa4ef8bd3dd49de48f71d9e
Eduard Carvajal - Ceiba Software <eduard.carvajal@ceiba.com.co>
2019-12-17 08:04:00 -0500

    Se agrega la opción de refrescar la vista del resumen de la cuenta

--- ServicioBanoClient/src/app/features/cuenta/cobrar-cuenta/cobrar-cuenta.component.ts
+++ ServicioBanoClient/src/app/features/cuenta/cobrar-cuenta/cobrar-cuenta.component.ts
@@ -6,6 +6,7 @@ import { Cobrar } from '../../../core/modelo/Cobrar';
 import { Cuenta } from 'src/app/core/modelo/Cuenta';
 import { EventoAlertService, Alert } from '../../../shared/eventos/evento-alert.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { faSyncAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-cobrar-cuenta',
@@ -17,17 +18,14 @@ export class CobrarCuentaComponent implements OnInit {
   @ViewChild('ocultarModalCobrar', { static: false })
   closeModal: ElementRef;
   cobrar: Cobrar = new Cobrar(new Cuenta(0, 0, '', '', 0, 0), 0, 0, 0, 0, 0, 0, 0, 0, 0);
+  faSyncAlt = faSyncAlt;
 
   constructor(private gestion: GestionCuentaService, private eventCobrar: EventoCobrarCuentaService,
               private eventAlert: EventoAlertService, private router: Router, private activateRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.eventCobrar.changeEmitted$.subscribe(id => {
-      this.gestion.cobrarCuenta(id).subscribe({
-        next: (comando: ComandoRespuestaCobrar) => {
-          this.cobrar = comando.respuesta;
-        }
-      });
+      this.cargarCuenta(id);
     });
   }
 
@@ -44,6 +42,18 @@ export class CobrarCuentaComponent implements OnInit {
         this.closeModal.nativeElement.click();
         this.eventAlert.emitChange(new Alert(tipoAlerta, mensaje));
         this.router.navigate(['listarbano'], { relativeTo: this.activateRoute });
+      }
+    });
+  }
+
+  public refrescarCuenta(id: number) {
+    this.cargarCuenta(id);
+  }
+
+  private cargarCuenta(id: number) {
+    this.gestion.cobrarCuenta(id).subscribe({
+      next: (comando: ComandoRespuestaCobrar) => {
+        this.cobrar = comando.respuesta;
       }
     });
   }
